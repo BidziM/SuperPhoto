88bf67326ef7c510c781a10e92f4d88910e85978
TheBidzi <miloszbagrowski96@gmail.com>
2022-04-23 13:41:14 +0200

    Add Prettier

--- frontend/src/components/EditObj.js
+++ frontend/src/components/EditObj.js
@@ -1,49 +1,63 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useCanvas } from "../context/CanvasContext";
-import TextField from '@mui/material/TextField';
-import Slider from '@mui/material/Slider';
+import TextField from "@mui/material/TextField";
+import Slider from "@mui/material/Slider";
 
 function EditBar() {
-    const {
-        selectedObj,
-        editItem
-      } = useCanvas();
-    const [parameters, setParameters] = useState({...selectedObj})
+  const { selectedObj, editItem } = useCanvas();
+  const [parameters, setParameters] = useState({ ...selectedObj });
 
-    useEffect(() => {
-        setParameters({...selectedObj})
-    }, [selectedObj])
-    
-    const changeParameters = (e) => {
-        const {name, value} = e.target
-        const newParameters = {...parameters, [name]: value}
-        setParameters(newParameters);
-        editItem(newParameters)
-    }
+  useEffect(() => {
+    setParameters({ ...selectedObj });
+  }, [selectedObj]);
 
-  if (!selectedObj) return null
+  const changeParameters = (e) => {
+    const { name, value } = e.target;
+    const newParameters = { ...parameters, [name]: value };
+    setParameters(newParameters);
+    editItem(newParameters);
+  };
+
+  if (!selectedObj) return null;
   return (
     <Wrapper>
-        <TextField id="standard-basic" label="Name" variant="standard" name={"name"} value={parameters.name} onChange={changeParameters}/>
-        <Slider
-            size="small"
-            defaultValue={50}
-            aria-label="Small"
-            valueLabelDisplay="auto"
-            onChange={changeParameters}
-            name="size"
-         />
-        <TextField value={parameters.x} label="X" name="x" variant="standard"  onChange={changeParameters}/>
-        <TextField value={parameters.y} label="Y" name="y" variant="standard" onChange={changeParameters}/>
+      <TextField
+        id="standard-basic"
+        label="Name"
+        variant="standard"
+        name={"name"}
+        value={parameters.name}
+        onChange={changeParameters}
+      />
+      <Slider
+        size="small"
+        defaultValue={50}
+        aria-label="Small"
+        valueLabelDisplay="auto"
+        onChange={changeParameters}
+        name="size"
+      />
+      <TextField
+        value={parameters.x}
+        label="X"
+        name="x"
+        variant="standard"
+        onChange={changeParameters}
+      />
+      <TextField
+        value={parameters.y}
+        label="Y"
+        name="y"
+        variant="standard"
+        onChange={changeParameters}
+      />
     </Wrapper>
   );
 }
 
 export default EditBar;
 
-
 const Wrapper = styled.div`
-    width: 200px;
+  width: 200px;
 `;
-
